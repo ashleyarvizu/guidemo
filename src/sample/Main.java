e2a0a7e1eaa4deacf14e902026c54dfe3b49d685
kch90 <32426748+kch90@users.noreply.github.com>
2018-04-02 17:33:35 -0500

    updates

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1300, 900));
+        primaryStage.setScene(new Scene(root, 800, 900));
         primaryStage.show();
     }
 
